8fd763b6c64a79f087528720b55ee15d3bb5392e
NarenderSD <narendersdcs@gmail.com>
2023-06-28 21:39:48 +0530

    URL to QR code

---
+++ Python Project/QR Code.py
@@ -0,0 +1,20 @@
+import qrcode
+
+def generate_qr_code(url, filename):
+    qr = qrcode.QRCode(
+        version=1,
+        error_correction=qrcode.constants.ERROR_CORRECT_H,
+        box_size=10,
+        border=4,
+    )
+    qr.add_data(url)
+    qr.make(fit=True)
+
+    img = qr.make_image(fill_color="black", back_color="white")
+    img.save(filename)
+
+# Example usage
+url = "https://example.com"
+filename = "qrcode.png"
+generate_qr_code(url, filename)
+print(f"QR code saved as {filename}")
